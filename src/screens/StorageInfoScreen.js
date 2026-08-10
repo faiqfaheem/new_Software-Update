23196a36c774c941689a27eb861f6211a1bc306e
HASSAN-RAMZAN59 <hasaanramzan59@gmail.com>
2026-08-10 17:05:23 +0500

    feat: update storage breakdown icons with SVG assets and target API 37

--- src/screens/StorageInfoScreen.js
+++ src/screens/StorageInfoScreen.js
@@ -61,6 +61,62 @@ const formatBytes = (bytes) => {
   return `${mb.toFixed(2)} MB`;
 };
 
+const ALL_APPS_SVG = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_124_1084)">
+<path d="M44.6406 22.75H33.6094C30.3785 22.75 27.75 20.1215 27.75 16.8906V5.85938C27.75 2.62852 30.3785 0 33.6094 0H44.6406C47.8715 0 50.5 2.62852 50.5 5.85938V16.8906C50.5 20.1215 47.8715 22.75 44.6406 22.75Z" fill="#FF3939"/>
+<path d="M16.8906 22.75H5.85937C2.62852 22.75 0 20.1215 0 16.8906V5.85938C0 2.62852 2.62852 0 5.85937 0H16.8906C20.1215 0 22.75 2.62852 22.75 5.85938V16.8906C22.75 20.1215 20.1215 22.75 16.8906 22.75Z" fill="#7EE780"/>
+<path d="M16.8906 50.5H5.85938C2.62852 50.5 0 47.8715 0 44.6406V33.6094C0 30.3785 2.62852 27.75 5.85938 27.75H16.8906C20.1215 27.75 22.75 30.3785 22.75 33.6094V44.6406C22.75 47.8715 20.1215 50.5 16.8906 50.5Z" fill="#7ED8F6"/>
+<path d="M44.6406 50.5H33.6094C30.3785 50.5 27.75 47.8715 27.75 44.6406V33.6094C27.75 30.3785 30.3785 27.75 33.6094 27.75H44.6406C47.8715 27.75 50.5 30.3785 50.5 33.6094V44.6406C50.5 47.8715 47.8715 50.5 44.6406 50.5Z" fill="#FFD92E"/>
+</g>
+<defs>
+<clipPath id="clip0_124_1084">
+<rect width="50" height="50" fill="white"/>
+</clipPath>
+</defs>
+</svg>`;
+
+const IMAGES_SVG = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_124_1076)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M47.5 40C48.8807 40 50 38.8807 50 37.5V2.5C50 1.11929 48.8807 0 47.5 0H12.5C11.1193 0 10 1.11929 10 2.5V37.5C10 38.8807 11.1193 40 12.5 40H47.5ZM16.0659 35C15.433 35 15.0003 34.5225 15 34.009C15.0001 33.8097 15.0652 33.6043 15.2087 33.42L22.8221 25.318C23.2292 24.898 23.9364 24.898 24.3649 25.298L27.8577 29L34.4147 19.438C34.8647 18.838 35.8075 18.858 36.2145 19.478L44.8695 33.5C45.255 34.18 44.7408 35 43.9265 35H16.0659ZM27.5 12.5C27.5 15.2614 25.2615 17.5 22.5 17.5C19.7386 17.5 17.5 15.2614 17.5 12.5C17.5 9.73857 19.7386 7.5 22.5 7.5C25.2615 7.5 27.5 9.73857 27.5 12.5Z" fill="url(#paint0_linear_124_1076)"/>
+<path d="M0 11.25C0 9.17893 1.67893 7.5 3.75 7.5H5V45H42.5V46.25C42.5 48.321 40.821 50 38.75 50H3.75C1.67893 50 0 48.321 0 46.25V11.25Z" fill="url(#paint1_linear_124_1076)"/>
+</g>
+<defs>
+<linearGradient id="paint0_linear_124_1076" x1="25" y1="0" x2="25" y2="50" gradientUnits="userSpaceOnUse">
+<stop stop-color="#EE9AE5"/>
+<stop offset="1" stop-color="#5961F9"/>
+</linearGradient>
+<linearGradient id="paint1_linear_124_1076" x1="25" y1="-8.76539e-08" x2="25" y2="50" gradientUnits="userSpaceOnUse">
+<stop stop-color="#EE9AE5"/>
+<stop offset="1" stop-color="#5961F9"/>
+</linearGradient>
+<clipPath id="clip0_124_1076">
+<rect width="50" height="50" fill="white"/>
+</clipPath>
+</defs>
+</svg>`;
+
+const AUDIOS_SVG = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M28.7626 3.61858L12.9154 14.7408H4.39453C1.97129 14.7408 0 16.7121 0 19.1354V30.8541C0 33.2774 1.97129 35.2487 4.39453 35.2487H12.9154L28.9257 46.471C31.8359 47.9862 35.3516 45.8735 35.3516 42.5729V7.41662C35.3516 4.25783 31.9923 1.87453 28.7626 3.61858Z" fill="url(#paint0_linear_124_1054)"/>
+<path d="M39.7461 14.7402C38.9364 14.7402 38.2812 15.3954 38.2812 16.2051C38.2812 17.0147 38.9364 17.6699 39.7461 17.6699C43.7858 17.6699 47.0703 20.9558 47.0703 24.9941C47.0703 29.0325 43.7858 32.3184 39.7461 32.3184C38.9364 32.3184 38.2812 32.9735 38.2812 33.7832C38.2812 34.5929 38.9364 35.248 39.7461 35.248C45.3995 35.248 50 30.6476 50 24.9941C50 19.3407 45.3995 14.7402 39.7461 14.7402ZM39.7461 26.459C38.9364 26.459 38.2812 27.1142 38.2812 27.9238C38.2812 28.7335 38.9364 29.3887 39.7461 29.3887C42.1693 29.3887 44.1406 27.4174 44.1406 24.9941C44.1406 22.5709 42.1693 20.5996 39.7461 20.5996C38.9364 20.5996 38.2812 21.2548 38.2812 22.0645C38.2812 22.8741 38.9364 23.5293 39.7461 23.5293C40.5529 23.5293 41.2109 24.1859 41.2109 24.9941C41.2109 25.8023 40.5529 26.459 39.7461 26.459ZM13.3789 20.5996C12.5692 20.5996 11.9141 21.2548 11.9141 22.0645V27.9238C11.9141 28.7335 12.5692 29.3887 13.3789 29.3887C14.1886 29.3887 14.8438 28.7335 14.8438 27.9238V22.0645C14.8438 21.2548 14.1886 20.5996 13.3789 20.5996Z" fill="url(#paint1_linear_124_1054)"/>
+<defs>
+<linearGradient id="paint0_linear_124_1054" x1="17.6758" y1="46.9705" x2="17.6758" y2="3.02932" gradientUnits="userSpaceOnUse">
+<stop stop-color="#A93AFF"/>
+<stop offset="1" stop-color="#FF81FF"/>
+</linearGradient>
+<linearGradient id="paint1_linear_124_1054" x1="30.957" y1="35.248" x2="30.957" y2="14.7402" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FFBEF9"/>
+<stop offset="1" stop-color="#FFF1FF"/>
+</linearGradient>
+</defs>
+</svg>`;
+
+const VIDEOS_SVG = `<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.25002 15.1035C4.23648 15.1035 2.60419 16.7358 2.60419 18.7493V41.666C2.60419 43.6796 4.23648 45.3119 6.25002 45.3119H43.75C45.7636 45.3119 47.3959 43.6796 47.3959 41.666V18.7493C47.3959 16.7358 45.7636 15.1035 43.75 15.1035H6.25002Z" fill="#EE4D4D"/>
+<path d="M21.7294 21.636C21.2521 21.3019 20.6285 21.261 20.1117 21.5302C19.595 21.7991 19.2708 22.3333 19.2708 22.916V37.4994C19.2708 38.0821 19.595 38.6162 20.1117 38.8852C20.6285 39.1544 21.2521 39.1135 21.7294 38.7794L32.146 31.4877C32.5637 31.1954 32.8125 30.7175 32.8125 30.2077C32.8125 29.6979 32.5637 29.22 32.146 28.9277L21.7294 21.636Z" fill="#ECEFF1"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M6.77081 11.459C6.77081 10.596 7.47038 9.89648 8.33331 9.89648H41.6666C42.5296 9.89648 43.2291 10.596 43.2291 11.459C43.2291 12.3219 42.5296 13.0215 41.6666 13.0215H8.33331C7.47038 13.0215 6.77081 12.3219 6.77081 11.459Z" fill="#EE4D4D"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M10.9375 6.25C10.9375 5.38706 11.6371 4.6875 12.5 4.6875H37.5C38.3629 4.6875 39.0625 5.38706 39.0625 6.25C39.0625 7.11294 38.3629 7.8125 37.5 7.8125H12.5C11.6371 7.8125 10.9375 7.11294 10.9375 6.25Z" fill="#EE4D4D"/>
+</svg>`;
+
 const StorageInfoScreen = ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [permissionGranted, setPermissionGranted] = useState(false);
@@ -327,7 +383,7 @@ const StorageInfoScreen = ({ navigation }) => {
           {/* Item 1: Audios */}
           <View style={styles.breakdownCard}>
             <View style={styles.iconSquare}>
-              <Image source={AUDIO_ICON} style={{ width: 36, height: 36 }} resizeMode="contain" />
+              <SvgXml xml={AUDIOS_SVG} width={36} height={36} />
             </View>
             <Text style={styles.breakdownTitle}>Audios</Text>
             <Text style={styles.breakdownSize}>{storageData.audiosSizeFormatted}</Text>
@@ -336,7 +392,7 @@ const StorageInfoScreen = ({ navigation }) => {
           {/* Item 2: Videos */}
           <View style={styles.breakdownCard}>
             <View style={styles.iconSquare}>
-              <Image source={VIDEO_ICON} style={{ width: 36, height: 36 }} resizeMode="contain" />
+              <SvgXml xml={VIDEOS_SVG} width={36} height={36} />
             </View>
             <Text style={styles.breakdownTitle}>Videos</Text>
             <Text style={styles.breakdownSize}>{storageData.videosSizeFormatted}</Text>
@@ -345,7 +401,7 @@ const StorageInfoScreen = ({ navigation }) => {
           {/* Item 3: Images */}
           <View style={styles.breakdownCard}>
             <View style={styles.iconSquare}>
-              <Image source={IMAGE_ICON} style={{ width: 36, height: 36 }} resizeMode="contain" />
+              <SvgXml xml={IMAGES_SVG} width={36} height={36} />
             </View>
             <Text style={styles.breakdownTitle}>Images</Text>
             <Text style={styles.breakdownSize}>{storageData.imagesSizeFormatted}</Text>
@@ -354,7 +410,7 @@ const StorageInfoScreen = ({ navigation }) => {
           {/* Item 4: All Apps */}
           <View style={styles.breakdownCard}>
             <View style={styles.iconSquare}>
-              <Image source={APPS_ICON} style={{ width: 36, height: 36 }} resizeMode="contain" />
+              <SvgXml xml={ALL_APPS_SVG} width={36} height={36} />
             </View>
             <Text style={styles.breakdownTitle}>All Apps</Text>
             <Text style={styles.breakdownSize}>{storageData.allAppsSizeFormatted}</Text>
